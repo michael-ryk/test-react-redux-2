aa7be582af4787836f72dbd077378dfb4ebfd5ec
MichaelR <spider747@gmail.com>
2022-07-13 21:53:31 +0300

    replace single item with list of items

--- src/components/Shop/Products.js
+++ src/components/Shop/Products.js
@@ -1,16 +1,21 @@
 import ProductItem from './ProductItem';
 import classes from './Products.module.css';
+import listOfProducts from './listOfProducts';
 
 const Products = (props) => {
   return (
     <section className={classes.products}>
       <h2>Buy your favorite products</h2>
       <ul>
-        <ProductItem
-          title='Test'
-          price={6}
-          description='This is a first product - amazing!'
-        />
+        {listOfProducts.map(item => {
+          return (
+            <ProductItem key={item.id} 
+              title={item.title}
+              price={item.price}
+              description={item.description}
+            />
+          )
+        })}
       </ul>
     </section>
   );
---
+++ src/components/Shop/listOfProducts.js
@@ -0,0 +1,16 @@
+const listOfProducts = [
+  {
+    id: 1,
+    title: "Television",
+    price: 200,
+    description: "Nice Television to watch it on evening"
+  },
+  {
+    id: 2,
+    title: "Camera",
+    price: 20,
+    description: "Device to record stuff around you"
+  }
+];
+
+export default listOfProducts;
